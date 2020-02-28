e47ffa249a057a3881930d4a7467a1497c0b1b79
Maera-David <maera.david68@gmail.com>
2020-02-28 16:57:46 +0100

    Galerie non buguée

--- assets/portfolio.js
+++ assets/portfolio.js
@@ -1,15 +1,19 @@
+ $(document).ready(function() {
+    $('.menu-toggler').on('click', function() {
+        $(this).toggleClass('open');
+        $('.top-nav').toggleClass('open');
+    })
+});
+
 $(function() {
-    let selectedClass = "";
-
-        $(".filter").click(function(){
-        selectedClass = $(this).attr("data-rel");
-
-        $("#gallery").fadeTo(100, 0.1);
-        $("#gallery div").not("."+selectedClass).fadeOut().removeClass('animation');
-
-        setTimeout(function() {
-        $("."+selectedClass).fadeIn().addClass('animation');
-        $("#gallery").fadeTo(300, 1);
-     }, 300);
+    var selectedClass = "";
+    $(".filter").click(function(){
+    selectedClass = $(this).attr("data-rel");
+    $("#gallery").fadeTo(100, 0.1);
+    $("#gallery div").not("."+selectedClass).fadeOut().removeClass('animation');
+    setTimeout(function() {
+    $("."+selectedClass).fadeIn().addClass('animation');
+    $("#gallery").fadeTo(300, 1);
+    }, 300);
     });
- });
+    });
