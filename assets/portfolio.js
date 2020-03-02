9ce2454fb20bf717be59d649125c7c66f79210eb
Maera-David <maera.david68@gmail.com>
2020-03-02 10:00:37 +0100

    Suppression contact, réorganisation PA, modification Projets

--- assets/portfolio.js
+++ assets/portfolio.js
@@ -4,16 +4,3 @@
         $('.top-nav').toggleClass('open');
     })
 });
-
-$(function() {
-    var selectedClass = "";
-    $(".filter").click(function(){
-    selectedClass = $(this).attr("data-rel");
-    $("#gallery").fadeTo(100, 0.1);
-    $("#gallery div").not("."+selectedClass).fadeOut().removeClass('animation');
-    setTimeout(function() {
-    $("."+selectedClass).fadeIn().addClass('animation');
-    $("#gallery").fadeTo(300, 1);
-    }, 300);
-    });
-    });
